61debedc9b1e8ad0b6cceff2a48dd9567696cdc9
Shun Kakinoki <shunkakinoki@gmail.com>
2022-06-21 16:14:55 -0700

    chore: update

--- apps/og/cypress.config.js
+++ apps/og/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-    projectId: "6un49s",
+    projectId: "div8g5",
     runMode: 3,
   },
 });

3a896c8263938ee51c96fb728ffe4d65699599d5
Shun Kakinoki <shunkakinoki@gmail.com>
2022-07-08 00:57:09 -0700

    chore: update

--- apps/changelog/src/middleware.ts
+++ apps/changelog/src/middleware.ts
@@ -4,11 +4,6 @@ import { NextResponse } from "next/server";
 
 export const middleware = (req: NextRequest) => {
   const pathname = req.nextUrl.pathname;
-  const hostname = req.headers.get("host");
-
-  if (hostname.endsWith("vercel.app")) {
-    return;
-  }
 
   const isExcluded = RedirectPrefixes.find(path => {
     return pathname?.startsWith(path);

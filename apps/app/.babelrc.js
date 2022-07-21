1dc9c28c90186273e1974e443ff9380adf9f10bb
Shun Kakinoki <shunkakinoki@gmail.com>
2022-07-20 21:24:15 -0700

    chore: update

--- apps/app/.babelrc.js
+++ apps/app/.babelrc.js
@@ -1,9 +1,6 @@
 const shouldInstrumentCode = "CYPRESS_INSTRUMENT_CODE" in process.env;
-console.log("shouldInstrumentCode", shouldInstrumentCode);
 
 module.exports = {
   presets: ["next/babel"],
   plugins: shouldInstrumentCode ? ["istanbul"] : [],
 };
-
-console.dir(module.exports, { depth: null });
--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -30,6 +30,7 @@ const config = {
     ignoreDuringBuilds: true,
   },
   experimental: {
+    forceSwcTransforms: process.env.CYPRESS_INSTRUMENT_CODE !== 1,
     scrollRestoration: true,
   },
   publicRuntimeConfig: {

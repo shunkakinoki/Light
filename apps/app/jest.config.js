84d680373e5de1622db5644def51efde26f18fd8
Shun Kakinoki <shunkakinoki@gmail.com>
2022-07-21 12:35:19 -0700

    perf: ini test

--- apps/app/jest.config.js
+++ apps/app/jest.config.js
@@ -4,8 +4,9 @@ const createJestConfig = nextJest({
   dir: "./",
 });
 
-// Add any custom config to be passed to Jest
 const customJestConfig = {
+  collectCoverage: true,
+  collectCoverageFrom: ["./src/**"],
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
 };

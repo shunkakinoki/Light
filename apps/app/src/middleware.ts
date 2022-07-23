1fcc08ad9c13471fb427f3c479d6c97a65fee8c3
Shun Kakinoki <shunkakinoki@gmail.com>
2022-07-22 18:55:45 -0700

    chore: update

--- apps/app/src/middleware.ts
+++ apps/app/src/middleware.ts
@@ -1,16 +1,7 @@
-import {
-  // excludedMiddleware,
-  linksMiddleware,
-  homeMiddleware,
-  pipeMiddleware,
-} from "@lightdotso/middlewares";
+import { linksMiddleware, pipeMiddleware } from "@lightdotso/middlewares";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export const middleware = (req: NextRequest) => {
-  return pipeMiddleware(req, NextResponse.next(), [
-    // excludedMiddleware,
-    linksMiddleware,
-    homeMiddleware,
-  ]);
+  return pipeMiddleware(req, NextResponse.next(), [linksMiddleware]);
 };

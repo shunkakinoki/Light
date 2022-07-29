bb3e50282d69ae8f43a51e801ce4a7684a67e8b1
Shun Kakinoki <shunkakinoki@gmail.com>
2022-07-29 12:02:22 -0700

    fix: redirect middleware

--- packages/middlewares/src/home.ts
+++ packages/middlewares/src/home.ts
@@ -1,12 +1,15 @@
-import { SocialLinks } from "@lightdotso/const";
+import { RedirectPrefixes } from "@lightdotso/const";
 import type { ComposableMiddleware } from "next-compose-middleware";
 import { NextResponse } from "next/server";
 
 export const homeMiddleware: ComposableMiddleware = async (req, res) => {
   const pathname = req.nextUrl.pathname;
+  const isExcluded = RedirectPrefixes.find(path => {
+    return pathname?.startsWith(path);
+  });
 
-  if (pathname === "/home") {
-    return NextResponse.redirect(SocialLinks.Website);
+  if (isExcluded) {
+    return NextResponse.redirect(`https://light.so${pathname}`);
   }
 
   return res;

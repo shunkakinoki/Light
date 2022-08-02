f261535d66668b393ab0d731baf41af1ce1d919f
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-01 19:38:08 -0700

    chore: udpate

--- contracts/renderer/src/Renderer.sol
+++ contracts/renderer/src/Renderer.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.13;
 
 contract Renderer {
   string private greeting;
--- contracts/renderer/test/Renderer.t.sol
+++ contracts/renderer/test/Renderer.t.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "@lightdotso/renderer/Renderer.sol";

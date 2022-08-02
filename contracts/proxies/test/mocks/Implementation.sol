8e29072976ca3770698b115f82c6e8d75a08aefa
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-01 19:18:31 -0700

    chore: update

--- contracts/proxies/test/mocks/Implementation.sol
+++ contracts/proxies/test/mocks/Implementation.sol
@@ -5,11 +5,5 @@ pragma solidity ^0.8.13;
 import { Initializable } from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
 contract Implementation is Initializable {
-  uint256 internal _value;
-
   function initialize() public initializer {}
-
-  function setValue(uint256 _number) public {
-    _value = _number;
-  }
 }

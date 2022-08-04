e4639cf1806aee4c504c43cdec18a634afd32959
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-03 17:27:58 -0700

    chore: update

--- contracts/protocol/src/LightOrb.sol
+++ contracts/protocol/src/LightOrb.sol
@@ -3,5 +3,13 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract LightOrb is ERC721Upgradeable {}
+contract LightOrb is ERC721Upgradeable, UUPSUpgradeable, OwnableUpgradeable {
+  function initialize() external initializer {
+    OwnableUpgradeable.__Ownable_init();
+  }
+
+  function _authorizeUpgrade(address) internal override onlyOwner {}
+}

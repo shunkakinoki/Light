9a2dc3b1a751937b26a7cd498c75d2a75a657c77
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-09 09:53:13 -0700

    chore: update

--- contracts/protocol/src/storages/LightOrbFactoryStorage.sol
+++ contracts/protocol/src/storages/LightOrbFactoryStorage.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.13;
 
 import { UpgradeableBeacon } from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 
+/// @title Storage contract for the LightOrb contract.
+/// @author Shun Kakinoki
+/// @notice This contract is used to manage the storage of the UUPS upgradeable contract.
 contract LightOrbFactoryStorage {
   UpgradeableBeacon public upgradeableBeacon;
 

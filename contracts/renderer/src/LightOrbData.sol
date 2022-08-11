0dc4062c40c186b584ddcfe2a3d338300585dded
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-10 18:31:04 -0700

    chore: update

---
+++ contracts/renderer/src/LightOrbData.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.13;
+
+/// @title Library for storing light orbs data
+/// @author Shun Kakinoki <shunkakinoki@gmail.com>
+/// @author Design by Oz Hashimoto <hello@okazu.co>
+library LightOrbData {
+  /// 16 types
+  enum BazelId {
+    Special,
+    Gold,
+    Silver,
+    MetalGray,
+    MetalBlack,
+    White,
+    Pink,
+    Purple,
+    Turqoise,
+    Blue,
+    SkyBlue,
+    Teal,
+    Green,
+    Yellow,
+    Orange,
+    Red
+  }
+
+  /// 16 types
+  enum OrbId {
+    Pink,
+    Purple,
+    Blue,
+    SkyBlue,
+    Teal,
+    Green,
+    Yellow,
+    Orange,
+    Red,
+    Gray,
+    DeepPurple,
+    Gold,
+    EverGreen,
+    Emerald,
+    Sapphire,
+    Diamond
+  }
+}

7d643c06003fa3f4a5881c500142b52448ddf4e5
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-15 14:31:30 -0700

    chore: update

--- packages/services/src/galaxy.ts
+++ packages/services/src/galaxy.ts
@@ -4,7 +4,7 @@ import type { GalaxyOats } from "@lightdotso/types";
 import { fetcher } from "./fetcher";
 
 export const fetchGalaxyOats = (address: string): Promise<GalaxyOats> => {
-  return fetcher(`${ApiLinks.CACHE}/network/${address}`, {
+  return fetcher(`${ApiLinks.CACHE}/galaxy/${address}`, {
     method: "GET",
   });
 };

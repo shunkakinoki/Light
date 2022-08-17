ab9ca956815697d3a9d4ebed33edf78b1cd0deba
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-17 00:15:37 -0700

    chore: update

--- apps/changelog/src/pages/_app.tsx
+++ apps/changelog/src/pages/_app.tsx
@@ -8,8 +8,19 @@ import {
 import type { AppProps } from "next/app";
 import type { FC } from "react";
 import "@lightdotso/changelog/styles/index.css";
+import { useEffect, useState } from "react";
 
 const CustomApp: FC<AppProps> = ({ Component, pageProps }) => {
+  const [showChild, setShowChild] = useState(false);
+
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+
   return (
     <ClientOnly>
       <ThemeScript />

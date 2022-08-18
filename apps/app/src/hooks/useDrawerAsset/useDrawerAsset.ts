4bc20100c82897bc5bd7505bef748c10d7f40654
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-18 15:39:24 -0700

    chore: update

--- apps/app/src/hooks/useDrawerAsset/useDrawerAsset.ts
+++ apps/app/src/hooks/useDrawerAsset/useDrawerAsset.ts
@@ -1,3 +1,5 @@
+import { useRouter } from "next/router";
+import { useCallback, useEffect } from "react";
 import { useRecoilState } from "recoil";
 
 import { drawerAssetAtom } from "@lightdotso/app/atoms/drawerAsset";
@@ -5,13 +7,21 @@ import { drawerAssetAtom } from "@lightdotso/app/atoms/drawerAsset";
 export const useDrawerAsset = () => {
   const [drawerAssetState, setDrawerAssetState] =
     useRecoilState(drawerAssetAtom);
+  const router = useRouter();
 
-  const closeDrawerAsset = () => {
+  const closeDrawerAsset = useCallback(() => {
     setDrawerAssetState({
       ...drawerAssetState,
       open: false,
     });
-  };
+  }, [drawerAssetState, setDrawerAssetState]);
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", closeDrawerAsset);
+    return () => {
+      router.events.off("routeChangeComplete", closeDrawerAsset);
+    };
+  }, [closeDrawerAsset, router.events]);
 
   return {
     drawerAssetState,

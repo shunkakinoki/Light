7d5743dadfe37df45cf35f9bd18bf94e1366ae97
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-22 09:59:56 -0700

    chore: update

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   "*.{js,ts,tsx}": ["yarn run eslint:cmd --fix"],
   "*.{md,json,yml}": ["yarn run prettier:cmd --write"],
-  "*.sol": ["yarn run forge:snapshot:cmd"],
+  "*.sol": ["yarn run prettier:cmd --write", "yarn run forge:snapshot:cmd"],
   "package.json": [
     "yarn run npm-package-json:lint",
     "yarn run prettier:cmd --write",

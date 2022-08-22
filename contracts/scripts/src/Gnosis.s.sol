b69dc8f0315fb89a06225fee1080853ddc923165
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-22 13:25:36 -0700

    chore: update

--- contracts/scripts/src/Gnosis.s.sol
+++ contracts/scripts/src/Gnosis.s.sol
@@ -13,20 +13,20 @@ interface GnosisProxyFactory {
 }
 
 contract GnosisScript is Script {
+  address GNOSIS_PROXY_FACTORY_ADDRESS_1_3_0 =
+    address(0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2);
   address GNOSIS_L1_SINGLETON_ADDRESS_1_3_0 =
     address(0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552);
-  address GNOSIS_L1_PROXY_FACTORY_ADDRESS_1_3_0 =
-    address(0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2);
   address GNOSIS_L2_SINGLETON_ADDRESS_1_3_0 =
     address(0x3E5c63644E683549055b9Be8653de26E0B4CD36E);
 
   function run() external {
     GnosisProxyFactory proxy = GnosisProxyFactory(
-      GNOSIS_L1_PROXY_FACTORY_ADDRESS_1_3_0
+      GNOSIS_PROXY_FACTORY_ADDRESS_1_3_0
     );
     vm.startBroadcast();
     proxy.createProxyWithNonce(
-      GNOSIS_L1_SINGLETON_ADDRESS_1_3_0,
+      GNOSIS_L2_SINGLETON_ADDRESS_1_3_0,
       abi.encodeWithSignature(
         "setup(address[],uint256,address,bytes,address,address,uint256,address)",
         [msg.sender],

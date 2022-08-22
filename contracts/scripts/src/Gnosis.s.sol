bb775d4afb2f14d9bb19ae03d9adeb3c8977934b
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-22 09:59:38 -0700

    chore: update

--- contracts/scripts/src/Gnosis.s.sol
+++ contracts/scripts/src/Gnosis.s.sol
@@ -4,10 +4,26 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 
-contract GnosisScript is Script {
-  function run() external {
-    vm.startBroadcast();
+interface GnosisProxyFactory {
+  function createProxyWithNonce(
+    address _singleton,
+    bytes memory initializer,
+    uint256 saltNonce
+  ) external;
+}
 
+contract GnosisL1Script is Script {
+  address GNOSIS_L1_SINGLETON_ADDRESS =
+    address(0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552);
+  address GNOSIS_L1_PROXY_FACTORY_ADDRESS_1_3_0 =
+    address(0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2);
+
+  function run() external {
+    GnosisProxyFactory proxy = GnosisProxyFactory(
+      GNOSIS_L1_PROXY_FACTORY_ADDRESS_1_3_0
+    );
+    vm.startBroadcast();
+    proxy.createProxyWithNonce(GNOSIS_L1_SINGLETON_ADDRESS, "", 0);
     vm.stopBroadcast();
   }
 }

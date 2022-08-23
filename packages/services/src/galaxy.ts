73cb734342ff9725d242b9f87d13478802bdb50d
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-22 17:21:12 -0700

    fix: galaxy oats parse

--- packages/services/src/galaxy.ts
+++ packages/services/src/galaxy.ts
@@ -2,15 +2,29 @@ import { ApiLinks } from "@lightdotso/const";
 import type { GalaxyOats, GalaxyCampaign } from "@lightdotso/types";
 
 import { fetcher } from "./fetcher";
+import type { Validator } from "./result";
+import { safeParse } from "./result";
 
-export const fetchGalaxyCampaign = (oatId: string): Promise<GalaxyCampaign> => {
+export const fetchGalaxyCampaign = (oatId: string) => {
   return fetcher(`${ApiLinks.CACHE}/galaxy/campaign/${oatId}`, {
     method: "GET",
   });
 };
 
-export const fetchGalaxyOats = (address: string): Promise<GalaxyOats> => {
+export const safeFetchGalaxyCampaign = (address: string) => {
+  return (validator?: Validator<GalaxyCampaign>) => {
+    return safeParse<GalaxyCampaign>(fetchGalaxyCampaign)(address)(validator);
+  };
+};
+
+export const fetchGalaxyOats = (address: string) => {
   return fetcher(`${ApiLinks.CACHE}/galaxy/${address}`, {
     method: "GET",
   });
+};
+
+export const safeFetchGalaxyOats = (address: string) => {
+  return (validator?: Validator<GalaxyOats>) => {
+    return safeParse<GalaxyOats>(fetchGalaxyOats)(address)(validator);
+  };
 };

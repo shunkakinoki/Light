7ecb22b0b15f89af58d699b0e22c30911a0ae2fa
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-22 17:16:24 -0700

    fix: opensea events

--- packages/services/src/opensea.ts
+++ packages/services/src/opensea.ts
@@ -8,7 +8,7 @@ import type {
 
 import { fetcher } from "./fetcher";
 import type { Validator } from "./result";
-import { fromPromise, zodValidate } from "./result";
+import { safeParse } from "./result";
 
 const ASSET = "/asset";
 const ASSETS = "/assets?owner=";
@@ -20,32 +20,20 @@ export const openseaHeaders = new Headers({
   "X-API-KEY": process.env.OPENSEA_API_KEY ?? "",
 });
 
-export const fetchOpenseaAsset = (
-  address: string,
-  tokenId: string,
-): Promise<OpenseaAsset> => {
+export const fetchOpenseaAsset = (address: string, tokenId: string) => {
   return fetcher(`${ApiLinks.OPEN_SEA}${ASSET}/${address}/${tokenId}`, {
     method: "GET",
     headers: openseaHeaders,
   });
 };
 
-export const safeFetchOpenseaAsset = async (
-  address: string,
-  tokenId: string,
-  validator?: Validator<OpenseaAsset>,
-) => {
-  const result = fromPromise<OpenseaAsset>(fetchOpenseaAsset(address, tokenId));
-  if (validator) {
-    return zodValidate(validator)(result);
-  }
-  return result;
+export const safeFetchOpenseaAsset = (spaceId: string) => {
+  return (validator?: Validator<OpenseaAsset>) => {
+    return safeParse<OpenseaAsset>(fetchOpenseaAsset)(spaceId)(validator);
+  };
 };
 
-export const fetchOpenseaAssets = (
-  address: string,
-  cursor?: string,
-): Promise<OpenseaAssets> => {
+export const fetchOpenseaAssets = (address: string, cursor?: string) => {
   return fetcher(
     `${ApiLinks.OPEN_SEA}${ASSETS}${address}${LIMIT}${
       cursor ? `${CURSOR}${cursor}` : ""
@@ -57,24 +45,15 @@ export const fetchOpenseaAssets = (
   );
 };
 
-export const safeFetchOpenseaAssets = async (
-  address: string,
-  cursor?: string,
-  validator?: Validator<OpenseaAssets>,
-) => {
-  const result = fromPromise<OpenseaAssets>(
-    fetchOpenseaAssets(address, cursor),
-  );
-  if (validator) {
-    return zodValidate(validator)(result);
-  }
-  return result;
+export const safeFetchOpenseaAssets = (address: string, cursor?: string) => {
+  return (validator?: Validator<OpenseaAssets>) => {
+    return safeParse<OpenseaAssets>(fetchOpenseaAssets)(address, cursor)(
+      validator,
+    );
+  };
 };
 
-export const fetchOpenseaEvents = (
-  address: string,
-  cursor?: string,
-): Promise<OpenseaEvents> => {
+export const fetchOpenseaEvents = (address: string, cursor?: string) => {
   return fetcher(
     `${ApiLinks.OPEN_SEA}${EVENTS}${address}${
       cursor ? `${CURSOR}${cursor}` : ""
@@ -86,13 +65,10 @@ export const fetchOpenseaEvents = (
   );
 };
 
-export const safeFetchOpenseaEvents = async (
-  address: string,
-  validator?: Validator<OpenseaEvents>,
-) => {
-  const result = fromPromise<OpenseaEvents>(fetchOpenseaEvents(address));
-  if (validator) {
-    return zodValidate(validator)(result);
-  }
-  return result;
+export const safeFetchOpenseaEvents = (address: string, cursor?: string) => {
+  return (validator?: Validator<OpenseaEvents>) => {
+    return safeParse<OpenseaEvents>(fetchOpenseaEvents)(address, cursor)(
+      validator,
+    );
+  };
 };

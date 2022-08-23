9891b80eebd5fb1a49ead4c73e58e0dfeb7fdbb6
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-22 17:18:38 -0700

    fix: network parse

--- packages/services/src/network.ts
+++ packages/services/src/network.ts
@@ -1,16 +1,30 @@
 import { ApiLinks } from "@lightdotso/const";
-import type { NetworkRaw } from "@lightdotso/types";
+import type { NetworkRaw, Network } from "@lightdotso/types";
 
 import { fetcher } from "./fetcher";
+import type { Validator } from "./result";
+import { safeParse } from "./result";
 
-export const fetchNetworks = (address: string): Promise<NetworkRaw> => {
+export const fetchNetworks = (address: string) => {
   return fetcher(`${ApiLinks.CACHE}/network/${address}`, {
     method: "GET",
   });
 };
 
-export const fetchNetworksRaw = (address: string): Promise<NetworkRaw> => {
+export const safeFetchNetworks = (address: string) => {
+  return (validator?: Validator<Network>) => {
+    return safeParse<Network>(fetchNetworks)(address)(validator);
+  };
+};
+
+export const fetchNetworksRaw = (address: string) => {
   return fetcher(`${ApiLinks.CACHE}/network/raw/${address}`, {
     method: "GET",
   });
+};
+
+export const safeFetchNetworksRaw = (address: string) => {
+  return (validator?: Validator<NetworkRaw>) => {
+    return safeParse<NetworkRaw>(fetchNetworksRaw)(address)(validator);
+  };
 };

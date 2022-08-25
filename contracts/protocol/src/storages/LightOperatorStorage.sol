ce6795b3bd9dc4b8dca63feb25e85a3a59e43fcb
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-25 11:28:22 -0700

    chore: update

---
+++ contracts/protocol/src/storages/LightOperatorStorage.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+import { UpgradeableBeacon } from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+
+/// @title Storage contract for the LightOperator contract.
+/// @author Shun Kakinoki
+/// @notice This contract is used to manage the storage of the UUPS upgradeable contract.
+contract LightOperatorStorage {
+  // mapping(address => mapping(address => mapping(uint256 => uint256)))
+  //   public permissionsOf;
+
+  uint256[49] private __gap;
+}

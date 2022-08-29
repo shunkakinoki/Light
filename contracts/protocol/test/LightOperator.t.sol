ca715bf9c4992698bd219a51f9f789273b76f36d
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-29 11:49:49 -0700

    perf: ini lightoperator test

---
+++ contracts/protocol/test/LightOperator.t.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.13;
+
+import "@lightdotso/foundry/BaseTest.sol";
+import "@lightdotso/protocol/LightOperator.sol";
+
+contract LightOperatorTest is BaseTest {
+  LightOperator private lightOperator;
+
+  function setUp() public {
+    setUpLightProxies();
+  }
+
+  function testLightOperatorProxyInitialize() public {
+    vm.expectEmit(true, true, false, true, address(wrappedLightOperator));
+    vm.expectEmit(true, false, false, true, address(wrappedLightOperator));
+    emit OwnershipTransferred(address(this), address(this));
+    emit Initialized(2);
+    wrappedLightOperator.initialize();
+  }
+
+  function testLightOperatorProxySlot() public {
+    testLightOperatorProxyInitialize();
+
+    _testProxyImplementationSlot(
+      address(proxyLightOperator),
+      address(implementationLightOperator)
+    );
+
+    // Initializable
+    _testArbitrarySlot(
+      address(proxyLightOperator),
+      bytes32(uint256(0)),
+      bytes32(uint256(2))
+    );
+    // OwnableUpgradeable
+    _testArbitrarySlot(
+      address(proxyLightOperator),
+      bytes32(uint256(51)),
+      bytes32(uint256(uint160(address(this))))
+    );
+    /// UUPSUpgradeable
+    _testArbitrarySlot(
+      address(proxyLightOperator),
+      bytes32(uint256(101)),
+      bytes32(uint256(0))
+    );
+  }
+}

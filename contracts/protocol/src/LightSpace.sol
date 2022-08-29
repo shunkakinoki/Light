63a19e55d91ae3d5d76b26a7851690a536ed45b7
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-29 12:52:14 -0700

    chore: update

--- contracts/protocol/src/LightSpace.sol
+++ contracts/protocol/src/LightSpace.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.13;
 
 import { ILightSpace } from "@lightdotso/protocol/interfaces/ILightSpace.sol";
+import { LightOperatable } from "@lightdotso/protocol/abstract/LightOperatable.sol";
 import { LightSpaceStorageV1 } from "@lightdotso/protocol/storages/LightSpaceStorage.sol";
 import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
@@ -12,11 +13,14 @@ import { ERC721Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC
 /// @title Space contract for the Light protocol.
 /// @dev Light spaces are a collective of orbs.
 /// @author Shun Kakinoki
+//// @notice Stores operator permissions for all addresses. Addresses can give permissions to any other address to take specific indexed actions on their behalf.
+//// @notice Implemented based of JBProjects at https://github.com/jbx-protocol/juice-contracts-v2/blob/main/contracts/JBProjects.sol (MIT License)
 contract LightSpace is
   Initializable,
   OwnableUpgradeable,
   UUPSUpgradeable,
   ERC721Upgradeable,
+  LightOperatable,
   LightSpaceStorageV1,
   ILightSpace
 {
@@ -43,7 +47,23 @@ contract LightSpace is
   /*                      EXTERNAL VIEWS                        */
   /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
+  /**
+    @notice The count for the total number of projects.
+  */
   function getCount() external view returns (uint256) {
     return count;
+  }
+
+  /**
+    @notice The metadata for each project, which can be used across several domains.
+    @param  _projectId The ID of the project to which the metadata belongs.
+    @param  _domain The domain within which the metadata applies. Applications can use the domain namespace as they wish.
+  */
+  function getMetadataContentOf(uint256 _projectId, uint256 _domain)
+    external
+    view
+    returns (string memory)
+  {
+    return metadataContentOf[_projectId][_domain];
   }
 }

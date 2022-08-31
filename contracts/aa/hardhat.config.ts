b348fc25fb99cb1c7577e2b4814eb9cd1731132f
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-31 13:11:57 -0700

    feat: ini aa

---
+++ contracts/aa/hardhat.config.ts
@@ -0,0 +1,64 @@
+import fs, { readFileSync } from "fs";
+
+import { TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS } from "hardhat/builtin-tasks/task-names";
+import type { HardhatUserConfig } from "hardhat/config";
+import { subtask } from "hardhat/config";
+import * as toml from "toml";
+
+subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS).setAction(
+  async (_, __, runSuper) => {
+    const paths = await runSuper();
+
+    return paths.filter((p: any) => {
+      return !p.endsWith(".t.sol");
+    });
+  },
+);
+
+let foundry = toml.parse(readFileSync("../../foundry.toml").toString());
+
+const getRemappings = () => {
+  return fs
+    .readFileSync("../../remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean)
+    .map(line => {
+      return line.trim().split("=");
+    });
+};
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: foundry?.profile?.default?.solc_version,
+    settings: {
+      optimizer: {
+        enabled: foundry?.profile?.default?.optimizer || true,
+        runs: foundry?.profile?.default?.optimizer_runs || 200,
+      },
+    },
+  },
+  defaultNetwork: "hardhat",
+  paths: {
+    root: "../..",
+    sources: "contracts/aa/src",
+  },
+  //@ts-expect-error
+  preprocess: {
+    eachLine: (hre: any) => {
+      return {
+        transform: (line: string) => {
+          if (line.match(/^\s*import /i)) {
+            getRemappings().forEach(([find, replace]) => {
+              if (line.match(find)) {
+                line = line.replace(find, replace);
+              }
+            });
+          }
+          return line;
+        },
+      };
+    },
+  },
+};
+
+export default config;

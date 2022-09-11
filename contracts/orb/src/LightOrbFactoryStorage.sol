ce13bc24a26dae07cfde051bfef81ebb8feb0753
Shun Kakinoki <shunkakinoki@gmail.com>
2022-09-10 19:12:14 -0700

    chore: update

--- contracts/orb/src/LightOrbFactoryStorage.sol
+++ contracts/orb/src/LightOrbFactoryStorage.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.16;
 
+import { EmptyUUPSBeaconStorage } from "@lightdotso/proxies/utils/EmptyUUPSBeaconStorage.sol";
 import { LightManager } from "@lightdotso/manager/LightManager.sol";
 
 import { UpgradeableBeacon } from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
@@ -10,10 +11,6 @@ import { UpgradeableBeacon } from "@openzeppelin/contracts/proxy/beacon/Upgradea
 /// @author Shun Kakinoki
 /// @notice This contract is used to manage the storage of the UUPS upgradeable contract.
 /// @notice V0 is necessary to inherit `EmptyUUPSBeacon` slot
-contract LightOrbFactoryStorageV0 {
-  UpgradeableBeacon public upgradeableBeacon;
-
-  uint256[49] private __gap;
-}
+contract LightOrbFactoryStorageV0 is EmptyUUPSBeaconStorage {}
 
 contract LightOrbFactoryStorageV1 is LightOrbFactoryStorageV0, LightManager {}

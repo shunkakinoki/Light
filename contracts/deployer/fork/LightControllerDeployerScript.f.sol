7138b04a9b8aef6dc00fac5aa4556eb1e78ce4ad
Shun Kakinoki <shunkakinoki@gmail.com>
2022-09-15 16:38:08 -0700

    chore: update

--- contracts/deployer/fork/LightControllerDeployerScript.f.sol
+++ contracts/deployer/fork/LightControllerDeployerScript.f.sol
@@ -3,15 +3,15 @@
 pragma solidity ^0.8.16;
 
 import "forge-std/Test.sol";
-import "@lightdotso/deployer/LightOrbDeployerScript.s.sol";
+import "@lightdotso/deployer/LightControllerDeployerScript.s.sol";
 
-contract LightOrbDeployerScriptForkTest is Test {
-  LightOrbDeployerScript script;
+contract LightControllerDeployerScriptForkTest is Test {
+  LightControllerDeployerScript script;
 
   function setUp() public {
     string memory GOERLI_RPC_URL = vm.envString("GOERLI_RPC_URL");
     vm.createSelectFork(GOERLI_RPC_URL);
-    script = new LightOrbDeployerScript();
+    script = new LightControllerDeployerScript();
   }
 
   function testForkRun() public {

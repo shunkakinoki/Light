c752d5f8d54e877329454c2bb47e66f3b9292bbd
Shun Kakinoki <shunkakinoki@gmail.com>
2022-09-15 17:20:35 -0700

    chore: update

--- contracts/deployer/src/LightControllerDeployerScript.s.sol
+++ contracts/deployer/src/LightControllerDeployerScript.s.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.16;
 
 import "forge-std/Script.sol";
+import "forge-std/StdJson.sol";
 import { EmptyUUPS } from "@lightdotso/empty/EmptyUUPS.sol";
 import { ILightController } from "@lightdotso/controller/ILightController.sol";
 import { LightController } from "@lightdotso/controller/LightController.sol";
@@ -28,6 +29,43 @@ contract LightControllerDeployerScript is Script {
 
     wrappedLightController = LightController(address(proxyLightController));
     wrappedLightController.initialize();
+
+    string memory root = vm.projectRoot();
+    string memory path = string.concat(root, "/contracts/config/mainnet.json");
+    string memory json = vm.readFile(path);
+
+    address proxyLightCore = stdJson.readAddress(json, ".LightCore");
+    address proxyLightOperatorStore = stdJson.readAddress(
+      json,
+      ".LightOperatorStore"
+    );
+    address proxyLightOrb = stdJson.readAddress(json, ".LightOrb");
+    address proxyLightOrbFactory = stdJson.readAddress(
+      json,
+      ".LightOrbFactory"
+    );
+    address proxyLightSpace = stdJson.readAddress(json, ".LightSpace");
+
+    wrappedLightController.setContractProxy(
+      keccak256("LightCore"),
+      proxyLightCore
+    );
+    wrappedLightController.setContractProxy(
+      keccak256("LightOperatorStore"),
+      proxyLightOperatorStore
+    );
+    wrappedLightController.setContractProxy(
+      keccak256("LightOrb"),
+      proxyLightOrb
+    );
+    wrappedLightController.setContractProxy(
+      keccak256("LightOrbFactory"),
+      proxyLightOrbFactory
+    );
+    wrappedLightController.setContractProxy(
+      keccak256("LightSpace"),
+      proxyLightSpace
+    );
 
     vm.stopBroadcast();
   }
